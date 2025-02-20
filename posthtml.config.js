33363b5c5f35263201435848c04b21045095eb58
Lê Thị Nhật Vy_23ITEB123 <vyltn.23ite@vku.udn.vn>
2025-02-21 01:40:21 +0700

    fix navbar

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,7 +1,12 @@
+const menuData = require("./src/data/menuData.json"); // Thêm .json vào đường dẫn
+
 module.exports = {
   plugins: {
     "posthtml-include": {
       root: "./src"
+    },
+    "posthtml-expressions": {
+      locals: menuData // Sử dụng dữ liệu từ JSON
     }
   }
 };
